d6de5046cb5471225b31c2e0fa221d98fa4f59ab
Victor <vicdasilva32@gmail.com>
2017-02-25 00:31:51 -0600

    Added sideBar activity

---
+++ outnaboutapp/app/src/test/java/mainapp/ProfileFragment.java
@@ -0,0 +1,13 @@
+package mainapp;
+
+/**
+ * Created by victordasilva on 2/24/17.
+ */
+import android.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+    //Constructor
+    public ProfileFragment(){
+
+    }
+}
