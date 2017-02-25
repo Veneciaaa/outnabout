47089ec8a65f9f29dfe798f454284162c8c398ed
wallinjosh <jwallin@iastate.edu>
2017-02-25 16:50:29 -0600

    Initial commit - outnaboutserver

---
+++ outnaboutserver/src/main/java/com/outnabout/outnaboutserver/OutnaboutserverApplication.java
@@ -0,0 +1,12 @@
+package com.outnabout.outnaboutserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutnaboutserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutnaboutserverApplication.class, args);
+	}
+}
