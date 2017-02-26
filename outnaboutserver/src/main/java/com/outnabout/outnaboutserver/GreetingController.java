8803b82059fcbc987156562da4616a81861e5901
wallinjosh <jwallin@iastate.edu>
2017-02-25 20:05:41 -0600

    Updated server

--- outnaboutserver/src/main/java/com/outnabout/outnaboutserver/GreetingController.java
+++ outnaboutserver/src/main/java/com/outnabout/outnaboutserver/GreetingController.java
@@ -3,20 +3,18 @@ package com.outnabout.outnaboutserver;
 /**
  * Created by Josh on 2/25/17.
  */
-import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    //private static final String template = "Hello, %s!";
+    //private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    @RequestMapping("/initiateactiveuser")
+    public ActiveUser greeting() {
+        return new ActiveUser(0, "default", 0.0, 0.0);
     }
 }
--- outnaboutserver/src/main/java/com/outnabout/outnaboutserver/OutnaboutserverApplication.java
+++ outnaboutserver/src/main/java/com/outnabout/outnaboutserver/OutnaboutserverApplication.java
@@ -1,6 +1,5 @@
 package com.outnabout.outnaboutserver;
 
-import com.outnabout.outnaboutserver.User.ActiveUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
